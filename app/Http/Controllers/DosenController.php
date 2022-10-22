c10389034983dc98105a05bc1770f5639b0de5c1
Umifl <umififalatifah@gmail.com>
2022-10-22 22:24:04 +0700

    ini tugas dua

---
+++ app/Http/Controllers/DosenController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Dosen;
+
+class DosenController extends Controller
+{
+        //Insert
+    public function insert(){
+        
+        //RAW
+        $sql = DB::insert("INSERT INTO dosen (nidn,nama,jenis_kelamin,alamat,tempat_lahir,tanggal_lahir,photo,created_at,updated_at) VALUES ('10079', 'Hilman M.Kom','Laki-Laki','Jl.segaran','Karawang','1995-08-06','PhotoHilman.png',now(),now())");
+        dump($sql);
+
+        //SB
+        $sql1 = DB::table('dosen')->insert(
+            [
+                'nidn' => '10087',
+                'nama' => 'Saman S.Kom',
+                'jenis_kelamin' => 'Laki-Laki',
+                'alamat' => 'Jl.Loji curug',
+                'tempat_lahir' => 'Karawang',
+                'tanggal_lahir' => '1987-09-15',
+                'photo' => 'photoSaman.png',
+                'created_at' => now(),
+                'updated_at' => now()
+            ]
+        );
+        dump($sql1);
+
+        //ELOQUENT
+        $sql2 = Dosen::create(
+            [
+                'nidn' => '10094',
+                'nama' => 'Kusmayanti spd',
+                'jenis_kelamin' => 'Perempuan',
+                'alamat' => 'Jl.proklamasi no.5',
+                'tempat_lahir' => 'Karawang',
+                'tanggal_lahir' => '1990-07-28',
+                'photo' => 'photoKusmayanti.png',
+                'created_at' => now(),
+                'updated_at' => now()
+            ]
+            );
+            dump($sql2);
+    }
+
+    //Select
+    public function select(){
+
+        //RAW
+        $sql = DB::select("SELECT * FROM dosen");
+        dd($sql);
+
+        //SB
+        $sql1 = DB::table('dosen')->get();
+        dd($sql1);
+
+        //ELOQUENT
+        $sql2 = Dosen::all();
+        dd($sql2);
+    }
+
+
+        //Update
+    public function update(){
+
+        // RAW
+        $sql = DB::update("UPDATE dosen SET alamat='JL.Kuningan No.02',updated_at=now() WHERE id=?",[1]);
+        dd($sql);
+
+        //SB
+        $sql1 = DB::table('dosen')
+        ->where('id','3')
+        ->update(
+            [
+                'alamat' => 'JL.Kuningan No.02',
+                'updated_at' => now(),
+            ]
+            );
+        dd($sql1);
+
+        #ELOQUENT
+        $sql2 = Dosen::where('id','1')->first()->update([
+            'alamat' => 'Jl.Kuningan No.02',
+            'updated_at' => now(),
+        ]);
+        dd($sql2);
+
+
+    }
+
+        //Delete
+    public function delete(){
+
+        //RAW
+        $sql = DB::delete("DELETE FROM dosen WHERE nidn=?",['10094']);
+        dd($sql);
+
+        //SB
+        $sql1 = DB::table('dosen')
+        ->where('nidn','10094')
+        ->delete();
+        dd($sql1);
+
+        //ELOQUENT
+        $sql2 = Dosen::where('nidn','10094')->delete();
+        dd($sql2);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MatakuliahController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(MatakuliahController::class)->group(function () {
+    Route::get('/insertmatkul', 'insert');
+    Route::get('/selectmatkul', 'select');
+    Route::get('/updatematkul', 'update');
+    Route::get('/deletematkul', 'delete');
+});
+
+Route::controller(MahasiswaController::class)->group(function () {
+    Route::get('/insertmhs', 'insert');
+    Route::get('/selectmhs', 'select');
+    Route::get('/updatemhs', 'update');
+    Route::get('/deletemhs', 'delete');
+});
+
+Route::controller(DosenController::class)->group(function () {
+    Route::get('/insertdosen', 'insert');
+    Route::get('/selectdosen', 'select');
+    Route::get('/updatedosen', 'update');
+    Route::get('/deletedosen', 'delete');
+});
+
+
+Route::get('/dosen', function () 
+{
+    $dosens = ["Saman","Acih","Aan","Dede","Hilman","Geri","Christa","Kusmayanti","Ghazali","Bram"];
+    $pelajaran = ["Cloud Computing","Framework","PBM","Struktur Data","ALjabar Linear","Fisika","B.Inggris","Pancasila","B.indonesia","Pendidikan Agama"];
+    return view('dosen.dosen')->with('dosens',$dosens)
+    ->with('pelajaran',$pelajaran);
+}); 
+Route::get('/mahasiswa', function ()
+{
+    $siswas = ["Umi Fifa Latifah","Andrian Nurian","Zahra","Ray Rizki","Lily","Adrian Wibisono","Wildi Baydowi","Dewi Anjani","Ade Suparlan","Riri Rizqiah"];
+    $nim = ["20036","20032","20018","20047","20118","20056","20078","20185","20112","20015"];
+    return view('mahasiswa.mahasiswa')->with('siswas',$siswas)
+    ->with('nim',$nim);
+});
+Route::get('/matakuliah', function ()
+ {
+    $matkuls = ["Cloud Computing","Framework","PBM","Struktur Data","Aljabar Linear","Fisika","B.Inggris","Pancasila","B.Indonesia","Pendidikan Agama"];
+    $semester = ["5","5","5","2","1","1","1","3","2","3"];
+    return view('matakuliah.matakuliah')->with('matkuls',$matkuls)
+    ->with('semester',$semester);
+
+});
